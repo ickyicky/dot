0f5e21404dc909e2cf1291e94bb4e4520f79cda0
doman <domanski@basement.pl>
2023-08-29 23:17:39 +0200

    tmux and nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,54 +1,67 @@
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		lazy = true,
-		config = function()
-			require("lsp-zero.settings").preset({})
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			{ "L3MON4D3/LuaSnip" },
-		},
-		config = function()
-			require("lsp-zero.cmp").extend()
-			local cmp = require("cmp")
-			local cmp_action = require("lsp-zero.cmp").action()
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    lazy = true,
+    config = function()
+      require("lsp-zero.settings").preset({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      { "L3MON4D3/LuaSnip" },
+    },
+    config = function()
+      require("lsp-zero.cmp").extend()
+      local cmp = require("cmp")
+      local cmp_action = require("lsp-zero.cmp").action()
 
-			cmp.setup({
-				mapping = {
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-f>"] = cmp_action.luasnip_jump_forward(),
-					["<C-b>"] = cmp_action.luasnip_jump_backward(),
+      cmp.setup({
+        mapping = {
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+          ["<C-b>"] = cmp_action.luasnip_jump_backward(),
           ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-				},
-			})
-		end,
-	},
+        },
+      })
+    end,
+  },
 
-	-- LSP
-	{
-		"neovim/nvim-lspconfig",
-		cmd = "LspInfo",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "williamboman/mason.nvim" },
-		},
-		config = function()
-			local lsp = require("lsp-zero")
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    cmd = "LspInfo",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "williamboman/mason.nvim" },
+    },
+    config = function()
+      local lsp = require("lsp-zero")
 
-			lsp.on_attach(function(client, bufnr)
-				lsp.default_keymaps({ buffer = bufnr })
-			end)
+      lsp.ensure_installed(
+        {
+          "pylsp",
+          "ruff_lsp",
+          "gopls",
+        }
+      )
 
-			require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+      lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({ buffer = bufnr })
+      end)
 
-			lsp.setup()
-		end,
-	},
+      nvim_lsp = require("lspconfig")
+      nvim_lsp.lua_ls.setup(lsp.nvim_lua_ls())
+      nvim_lsp.ruff_lsp.setup({})
+      nvim_lsp.gopls.setup({})
+      nvim_lsp.dartls.setup({})
+      nvim_lsp.pylsp.setup({})
+
+      lsp.setup()
+    end,
+  },
 }
